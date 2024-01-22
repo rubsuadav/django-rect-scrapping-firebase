4e688372a12dd89c6bb987d38e3fb17139220a14
rubsuadav <rsuarezdavid@gmail.com>
2024-01-22 18:52:43 +0100

    Refactor views.py to retrieve restaurant data with comments from Firestore

--- main/views.py
+++ main/views.py
@@ -1,8 +1,19 @@
-from django.http import HttpResponse
-from .scrapping import PopulateDatabase
+from django.http import JsonResponse
+from firebase import firestore
+from google.cloud.firestore_v1 import Query
 
 
 def index(request):
-    populator = PopulateDatabase()
-    populator.populate()
-    return HttpResponse("Hello, world. You're at the polls index.")
+    restaurante_col = firestore.collection(u'restaurantes')
+    restaurantes = firestore.collection(u'restaurantes').order_by(
+        u'nombre', direction=Query.ASCENDING).limit(30).stream()
+
+    restaurantes_con_comentarios = []
+    for restaurante in restaurantes:
+        res = restaurante.to_dict()
+        comentarios = restaurante_col.document(restaurante.id).collection(
+            u'comentarios').order_by(u'fecha', direction=Query.ASCENDING).stream()
+        res['comentarios'] = [comentario.to_dict()
+                              for comentario in comentarios]
+        restaurantes_con_comentarios.append(res)
+    return JsonResponse(restaurantes_con_comentarios, safe=False)
