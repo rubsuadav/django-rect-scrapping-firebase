b21f9498a79128c2d5095e078091bd5faf76749d
rubsuadav <rsuarezdavid@gmail.com>
2024-01-24 18:31:11 +0100

    implemented suscription payment with stripe/firestore-stripe-payments extension

--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
-from firebase import firestore
+from firebase import firestore, auth
 from google.cloud.firestore_v1 import Query
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 
 def get_restaurant_comments(restaurante_col, restaurantes):
@@ -26,19 +27,19 @@ class IndexView(APIView):
                 try:
                     page = int(page)
                     if int(page) < 1:  # Si la pagina es menor a 1
-                        return Response(data={"error": "Pagina debe de ser mayor o igual a 1"}, status=400)
+                        return Response(data={"error": "Pagina debe de ser mayor o igual a 1"}, status=status.HTTP_400_BAD_REQUEST)
                 except Exception:
-                    return Response(data={"error": "Pagina debe de ser un numero"}, status=400)
+                    return Response(data={"error": "Pagina debe de ser un numero"}, status=status.HTTP_400_BAD_REQUEST)
 
                 # 10 restaurantes por pagina
                 restaurantes = restaurante_col.order_by(
                     u'nombre', direction=Query.ASCENDING).limit(10).offset((int(page) - 1) * 10).stream()
-                return Response(data=get_restaurant_comments(restaurante_col, restaurantes), status=200)
+                return Response(data=get_restaurant_comments(restaurante_col, restaurantes), status=status.HTTP_200_OK)
 
             else:  # Sin paginacion
                 restaurantes = restaurante_col.order_by(
                     u'nombre', direction=Query.ASCENDING).stream()
-                return Response(data=get_restaurant_comments(restaurante_col, restaurantes), status=200)
+                return Response(data=get_restaurant_comments(restaurante_col, restaurantes), status=status.HTTP_200_OK)
 
         else:  # Detalle de un restaurante
             restaurante = restaurante_col.document(
@@ -49,6 +50,25 @@ class IndexView(APIView):
                     str(restaurant_id)).collection(u'comentarios').order_by(u'fecha', direction=Query.ASCENDING).stream()
                 res['comentarios'] = [comentario.to_dict()
                                       for comentario in comentarios]
-                return Response(data=res, status=200)
+                return Response(data=res, status=status.HTTP_200_OK)
             else:  # Si no existe el restaurante
-                return Response(data={'error': 'Restaurante no encontrado'}, status=404)
+                return Response(data={'error': 'Restaurante no encontrado'}, status=status.HTTP_404_NOT_FOUND)
+
+
+class SubscriptionView(APIView):
+    def post(self, request):
+        user = auth.sign_in_with_email_and_password(
+            request.data.get('email'), request.data.get('password'))
+
+        doc_ref = firestore.collection(u'customers', user["localId"], u'checkout_sessions').add({
+            'price': request.data.get('price'),
+            "trial_from_plan": True,
+            "allow_promotion_codes": True,
+            'success_url': 'https://example.com/success',
+            'cancel_url': 'https://example.com/cancel',
+        })
+        doc = doc_ref[1].get()
+        while 'url' not in doc.to_dict():
+            doc = doc_ref[1].get()
+        checkout_session = doc.to_dict()
+        return Response(data={"checkout_session": checkout_session["url"]}, status=status.HTTP_200_OK)
