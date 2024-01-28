cdd28ab3f48884c8c6693d2b3dc84775123c9cf3
rubsuadav <rsuarezdavid@gmail.com>
2024-01-28 13:36:07 +0100

    Added react-router-dom package and added the basics routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,40 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// local layout imports
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+// local pages imports
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Home from "./pages/Home";
+
+// Authentication and routers imports
+import { AuthContextProvider } from "./context/authContext";
+import PublicRouter from "./context/routers/PublicRouter";
+import PrivateRouter from "./context/routers/PrivateRouter";
 
 export default function App() {
-  return <div className="text-red-800 text-3xl text-center">App</div>;
+  return (
+    <div>
+      <AuthContextProvider>
+        <Router>
+          <Navbar />
+          <Routes>
+            {/* Public routes  =====================================*/}
+            <Route path="/" element={<PublicRouter />}>
+              <Route index element={<Home />} />
+            </Route>
+            {/* Login and register */}
+            <Route path="" element={<PrivateRouter />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Route>
+          </Routes>
+          <Footer />
+        </Router>
+      </AuthContextProvider>
+    </div>
+  );
 }
