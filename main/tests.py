ac833124e9177d4024a0a32764fe7e5d77fabc6d
rubsuadav <rsuarezdavid@gmail.com>
2024-02-13 00:48:31 +0100

    Added restaurant search test cases

---
+++ main/tests.py
@@ -0,0 +1,24 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+import json
+from firebase import auth
+
+
+def get_token(self):
+    response = self.client.post('/api/auth/login/', data=json.dumps({
+        "email": "localuser@gmail.com",
+        "password": "@Test1234"
+    }), content_type='application/json')
+    return response.data.get('access_token')
+
+
+# RESTAURANTS TEST CASES #
+class RestaurantsViewTestCase(APITestCase):
+    def test_search_restaurant(self):
+        response = self.client.get(
+            '/api/restaurants/search/?search=Pasteleria Baklava')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_search_restaurant_not_found(self):
+        response = self.client.get('/api/restaurants/search/?search=McDonalds')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
