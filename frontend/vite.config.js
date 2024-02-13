72d90de0f1b41eb4e7946331ecadbe474012bd18
rubsuadav <rsuarezdavid@gmail.com>
2024-02-13 14:15:25 +0100

    Configured vite for testing adding coverage reports configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,11 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: "happy-dom",
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "html", "json-summary", "cobertura"],
+    },
+  },
 });
